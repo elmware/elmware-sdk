36fa23f87101e44db00614476b5a2a2185ff0cf1
louis <louis@elmware.com>
2019-09-10 16:12:22 -0400

    bump patch number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="elmsdk",
-    version="0.3.0",
+    version="0.3.1",
     author="Elmware",
     author_email="support@elmware.com",
     description="SDK for running docker containers on elmware",
